39ecb9d568df5dcf7302d8c5d919b640053393dd
acheung-94 <acheung@ucdavis.edu>
2024-01-23 14:32:27 -0800

    define populate method iboard.rb

--- practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,6 +1,10 @@
 class Board
     def initialize(side)
+        raise "side has to be an even num" if side.odd? #size has to be even!
         @grid = Array.new(side) { Array.new(side) }
+        @pairs = (side * side) / 2 #num of pairs that we need.
+        str = "abcdefghijklmnopqrstuvwxyz"
+        @pairs_array = str[0...@pairs].split("")
     end
 
     def [](pos)
@@ -15,6 +19,21 @@ class Board
         @grid[row][col] = value
     end
 
+    def populate #test.populate
+        @pairs_array.each do |pair| #(a), b, c
+            i = 0
+            while i < 2
+                row = rand(0...@grid.length)
+                col = rand(0...@grid.length)
+                pos = [row, col] #1, 2
+                if @grid[row][col] == nil 
+                    self[pos] = pair # @grid[row][col]
+                    i += 1 #increment if placement successful
+                end
+            end
+        end
+        p @grid
+    end 
 end
 
 
@@ -22,8 +41,6 @@ end
 
 
 if __FILE__ == $PROGRAM_NAME
-    board_test = Board.new(3)
-    p board_test
-    p board_test[[0,0]] = 5
-    p board_test
+    board_test = Board.new(4)
+    board_test.populate
 end
